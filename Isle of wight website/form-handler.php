565bbd104538652776e52e796858140e7b2b2186
Joesdesigns101 <141266385+Joesdesigns101@users.noreply.github.com>
2023-08-02 18:50:14 +0100

    Add files via upload

---
+++ Isle of wight website/form-handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'https://www.guitarlessonsinsale.com/'
+
+$email_subject = 'Guitar Lesson Enquiry';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'guitarlessonsin@gmail.com';
+
+$headers = "From: $emai_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: Contact.html");
+
+
+?>
